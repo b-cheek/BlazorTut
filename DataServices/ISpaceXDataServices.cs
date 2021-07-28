1e14559a53d59776909e0823e0522cb6d168184a
Brayden Cheek <Brayden.Cheek@internal.iso-group.com>
2021-07-28 17:15:31 -0400

    initial commit (already finished) for Blazor tutorial w/ REST / GraphQL API by Les Jackson

---
+++ DataServices/ISpaceXDataServices.cs
@@ -0,0 +1,8 @@
+using System.Threading.Tasks;
+using BlazorAPIClient.Dtos;
+
+namespace BlazorAPIClient {
+    public interface ISpaceXDataService {
+        Task<LaunchDto[]> GetAllLaunches();
+    }
+}
---
+++ Pages/Launches.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using BlazorAPIClient.DataServices; //? He added off camera
+using BlazorAPIClient.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAPIClient.Pages {
+    public partial class Launches {
+        [Inject]
+        ISpaceXDataService SpaceXDataService { get; set; }
+
+        private LaunchDto[] launches;
+
+        protected override async Task OnInitializedAsync() {
+            launches = await SpaceXDataService.GetAllLaunches();
+        }
+    }
+}
